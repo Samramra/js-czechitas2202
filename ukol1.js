31877e10dec2ee91500dac7fdf8d412cbdb7ddd3
Samramra <sarka7091@gmail.com>
2022-02-22 18:36:34 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -1,14 +1,1 @@
-
-
-let jmeno = "Petra";
-let vek = 34
-let vaha = 65
-let vyska = 1.67
-
-let bmi = vaha/vyska**2
-
-console.log(
-  "jmenuji se" + jmeno +
-  ", je mi"+ vek +
-  "vazim" + vaha + 
-  "kg", a moje BMI je" + bmi);
+console.log(1 === 1);
