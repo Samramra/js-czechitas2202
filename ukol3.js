4016053c0900fa45665d8287050b949ac4f6b6d9
Samramra <sarka7091@gmail.com>
2022-02-22 19:31:31 +0100

    1 file modified

---
+++ ukol3.js
@@ -0,0 +1,18 @@
+let a = Number(prompt(prvni strana"));
+let b = Number(prompt(prvni strana"));
+let c = Number(prompt(prvni strana"));
+
+
+if {
+  (a > b) 
+  if (a + b > c) {
+    console.log("nejvetsi strana je A:" + a);
+}
+else {
+  console.log("nejvetsi strana je C:" + c);
+}
+else (a + b > c) {
+  console.log("nejvetsi strana je B:" + b);
+}
+else {
+console.log("nejvetsi strana je C:" + c);
