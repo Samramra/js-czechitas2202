696a44b799d5bb9cb7ed9b3f02e5fc7f90738b6a
Samramra <sarka7091@gmail.com>
2022-02-22 19:36:28 +0100

    1 file modified

--- ukol3.js
+++ ukol3.js
@@ -1,12 +1,13 @@
 let a = Number(prompt(prvni strana"));
-let b = Number(prompt(prvni strana"));
-let c = Number(prompt(prvni strana"));
+let b = Number(prompt(druhá strana"));
+let c = Number(prompt(třetí strana"));
 
 
-if {
-  (a > b) 
+if (a > b) {
   if (a + b > c) {
     console.log("nejvetsi strana je A:" + a);
+  if (b + c > a) {
+    console.log("trojuhelnik lze sestavit" + a);
 }
 else {
   console.log("nejvetsi strana je C:" + c);
