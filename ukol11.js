2d9f52780032f0c1a36e393666d9b24089fbd7c9
Samramra <sarka7091@gmail.com>
2022-02-22 19:04:46 +0100

    1 file added

---
+++ ukol11.js
@@ -0,0 +1,17 @@
+let vek = 18;
+
+//jsi plnoletý nebo  nejsi
+//true/false
+if (vyraz) {}
+//vykona se když je pravda
+
+else {}
+//vykona když je neprqavda
+
+if (vek >=18){
+  console.log(vyborne jsi plnolety")
+  }
+else {
+  console.log("nejsi plnolety")
+}
+
