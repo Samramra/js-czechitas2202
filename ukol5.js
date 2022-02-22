aabedfcd03ec0f15101d86194b0a41a6266998f6
Samramra <sarka7091@gmail.com>
2022-02-22 19:27:07 +0100

    1 file modified

--- ukol5.js
+++ ukol5.js
@@ -1,3 +1,18 @@
 let a = Number(prompt(prvni cislo"));
 let b = Number(prompt(prvni cislo"));
 let c = Number(prompt(prvni cislo"));
+
+
+if {
+  (a > b) 
+  if (a > c) {
+    console.log("nejvetsicislo je A:" + a);
+}
+else {
+  console.log("nejvetsicislo je C:" + c);
+}
+else (b > c) {
+  console.log("nejvetsicislo je B:" + b);
+}
+else {
+console.log("nejvetsicislo je C:" + c);
